ef109aabb5cd0e3e46de2b12db9e94cc9de14a00
Sora0613 <sora0613.contact@gmail.com>
2024-01-18 21:03:00 +0900

    デザインの修正。

--- resources/views/inventory/create.blade.php
+++ resources/views/inventory/create.blade.php
@@ -1,43 +1,48 @@
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>在庫登録</title>
-</head>
-<body>
-    <h1>在庫登録</h1>
-    <a href="{{ route('home') }}" class="btn btn-primary">Home</a>
-    <a href="{{ route('inventory.index') }}" class="btn btn-primary">在庫一覧</a>
-    <a href="{{ route('inventory.create') }}" class="btn btn-primary">在庫登録</a>
-    <a href="{{ route('inventory.search') }}" class="btn btn-primary">Jan検索</a>
+@extends('layouts.app')
 
-    <br>
-    <br>
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">在庫登録</div>
 
-    <form action="{{ route('inventory.store') }}" method="POST">
-        @csrf
+                    <div class="card-body">
+                        <a href="{{ route('home') }}" class="btn btn-primary">Home</a>
+                        <a href="{{ route('inventory.index') }}" class="btn btn-primary">在庫一覧</a>
+                        <a href="{{ route('inventory.create') }}" class="btn btn-primary">在庫登録</a>
+                        <a href="{{ route('inventory.search') }}" class="btn btn-primary">Jan検索</a>
+                        <br>
+                        <br>
+                        <form>
+                            <div class="form-group">
+                                <label for="name">商品名</label>
+                                <input type="text" name="name" id="name" class="form-control"
+                                       @isset($product_info)
+                                           value="{{ $product_info['hits'][0]['name'] }}"
+                                       @endisset>
 
-        <div>
-            <label for="name">商品名</label>
-            <input type="text" name="name" id="name"
-                   @isset($product_info)
-                       value="{{ $product_info['hits'][0]['name'] }}"
-                   @endisset>
+                            </div>
+                            <div class="form-group">
+                                <label for="JAN">JANコード</label>
+                                <input type="text" name="JAN" id="JAN" class="form-control"
+                                       @isset($product_info)
+                                           value="{{ $product_info['hits'][10]['janCode'] }}"
+                                       @endisset>
+                            </div>
+                            <div class="form-group">
+                                <label for="price">価格</label>
+                                <input type="text" name="price" id="price" class="form-control"
+                                       @isset($product_info)
+                                           value="{{ $product_info['hits'][2]['price'] }}"
+                                       @endisset>
+                            </div>
+                            <br>
+                            <button type="submit" class="btn btn-primary">登録</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-            <label for="JAN">JANコード</label>
-            <input type="text" name="JAN" id="JAN"
-                   @isset($product_info)
-                        value="{{ $product_info['hits'][10]['janCode'] }}"
-                   @endisset>
-        </div>
-        <div>
-            <label for="price">価格</label>
-            <input type="text" name="price" id="price"
-                   @isset($product_info)
-                       value="{{ $product_info['hits'][2]['price'] }}"
-                   @endisset>
-        </div>
-        <button type="submit">登録</button>
-    </form>
-</body>
-</html>
+    </div>
+@endsection
--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -1,48 +1,57 @@
-<!doctype html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>在庫一覧</title>
-</head>
+@extends('layouts.app')
 
-<body>
-    <h1>在庫一覧</h1>
-    <a href="{{ route('home') }}" class="btn btn-primary">Home</a>
-    <a href="{{ route('inventory.index') }}" class="btn btn-primary">在庫一覧</a>
-    <a href="{{ route('inventory.create') }}" class="btn btn-primary">在庫登録</a>
-    <a href="{{ route('inventory.search') }}" class="btn btn-primary">Jan検索</a>
-    <br>
-    <br>
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">在庫登録</div>
 
-    @isset($inventories)
-        <table border="1">
-            <tr>
-                <th>商品名</th>
-                <th>JANコード</th>
-                <th>価格</th>
-                <th>更新日時</th>
-                <th>編集</th>
-                <th>削除</th>
-            </tr>
-            @foreach ($inventories as $inventory)
-                <tr>
-                    <td>{{ $inventory->name }}</td>
-                    <td>{{ $inventory->JAN }}</td>
-                    <td>{{ $inventory->price ?? '' }}</td>
-                    <td>{{ $inventory->updated_at }}</td>
-                    <td>
-                        <a href="{{ route('inventory.edit', $inventory->id) }}">編集</a>
-                    </td>
-                    <td>
-                        <form action="{{ route('inventory.destroy', $inventory->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">削除</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
-    @endisset
-</body>
-</html>
+                    <div class="card-body">
+                        <a href="{{ route('home') }}" class="btn btn-primary">Home</a>
+                        <a href="{{ route('inventory.index') }}" class="btn btn-primary">在庫一覧</a>
+                        <a href="{{ route('inventory.create') }}" class="btn btn-primary">在庫登録</a>
+                        <a href="{{ route('inventory.search') }}" class="btn btn-primary">Jan検索</a>
+                        <br>
+                        <br>
+                        @isset($inventories)
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th scope="col">商品名</th>
+                                    <th scope="col">JANコード</th>
+                                    <th scope="col">価格</th>
+                                    <th scope="col">更新日時</th>
+                                    <th scope="col">編集</th>
+                                    <th scope="col">削除</th>
+                                </tr>
+                                </thead>
+                                @foreach($inventories as $inventory)
+                                    <tbody>
+                                    <tr>
+                                        <td>{{ $inventory->name }}</td>
+                                        <td>{{ $inventory->JAN }}</td>
+                                        <td>{{ $inventory->price ?? '' }}</td>
+                                        <td>{{ $inventory->updated_at }}</td>
+                                        <td>
+                                            <a href="{{ route('inventory.edit', $inventory->id) }}">編集</a>
+                                        </td>
+                                        <td>
+                                            <form action="{{ route('inventory.destroy', $inventory->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit">削除</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                @endforeach
+                            </table>
+                        @endisset
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
