e66d9f8a53a21de3ddaca273d96bf31f01d906f9
Sora0613 <sora0613.contact@gmail.com>
2024-01-19 13:59:57 +0900

    errorの処理を追加しておいた。

--- app/Lib/yahoo_api_jan_search.php
+++ app/Lib/yahoo_api_jan_search.php
@@ -8,6 +8,10 @@ class yahoo_api_jan_search
     {
         $url = "https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?appid=" . config('services.yahoo_api.client_id') ."&jan_code=" . $jan;
         $json = file_get_contents($url);
-        return json_decode($json, true);
+        try {
+            return json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+        } catch (\JsonException $e) {
+            return $e;
+        }
     }
 }
