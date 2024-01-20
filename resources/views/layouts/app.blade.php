becc4ae2d174a2b56cc2652284f9e02cd7aac706
Sora0613 <sora0613.contact@gmail.com>
2024-01-20 16:02:00 +0900

    navbarが非ログイン状態でも閲覧可能だったため、ログインしないと見れないように。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,20 +37,23 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-item nav-link" href="{{ route('inventory.index') }}">在庫一覧</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-item nav-link" href="{{ route('inventory.create') }}">在庫登録</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-item nav-link" href="{{ route('inventory.search') }}">JAN検索</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-item nav-link" href="{{ route('collaborators.index') }}">共有管理</a>
-                        </li>
-                    </ul>
+                    @guest
+                        @else
+                            <ul class="navbar-nav me-auto">
+                                <li class="nav-item">
+                                    <a class="nav-item nav-link" href="{{ route('inventory.index') }}">在庫一覧</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-item nav-link" href="{{ route('inventory.create') }}">在庫登録</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-item nav-link" href="{{ route('inventory.search') }}">JAN検索</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-item nav-link" href="{{ route('collaborators.index') }}">共有管理</a>
+                                </li>
+                            </ul>
+                    @endguest
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
