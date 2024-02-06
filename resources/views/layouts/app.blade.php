97c2b7b05b7526a6b6b4d4f66a21928a6b42b6d0
Sora0613 <sora0613.contact@gmail.com>
2024-02-06 17:16:01 +0900

    upd. LINE LOGINのデザインを用件に合わせて設定。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,25 @@
             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </symbol>
     </svg>
+
+    <style>
+        .image-button {
+            background-image: url('storage/btn_login_base.png');
+            width: 142px; /* 画像の幅に合わせて調整 */
+            height: 40px; /* 画像の高さに合わせて調整 */
+            border: none;
+            cursor: pointer;
+            background-color: transparent;
+        }
+
+        .image-button:hover {
+            background-image: url('storage/btn_login_hover.png');
+        }
+
+        .image-button:active {
+            background-image: url('storage/btn_login_press.png');
+        }
+    </style>
 </head>
 <body>
     <div id="app">
