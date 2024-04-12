c0f864e511b974a568e6e44a8b5ff6bdc4e12395
Sora0613 <sora0613.contact@gmail.com>
2024-04-12 21:02:52 +0900

    fix. スマホで見た時のリストのデザインが崩れていたのの修正。

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -21,80 +21,84 @@
                                     在庫情報がありません - <a href="{{ route('inventory.create') }}" class="alert-link">在庫登録へ</a>
                                 </div>
                             @else
-                                <table class="table">
-                                    <thead>
-                                    <tr>
-                                        <th scope="col">商品名</th>
-                                        <th scope="col">JANコード</th>
-                                        <th scope="col">数</th>
-                                        <th scope="col" class="text-nowrap">価格</th>
-                                        <th scope="col">賞味期限</th>
-                                        <th scope="col">作成者</th>
-                                        <th scope="col">更新日時</th>
-                                        <th scope="col">編集</th>
-                                        <th scope="col">削除</th>
-                                    </tr>
-                                    </thead>
-                                    @foreach($inventories as $inventory)
-                                        <tbody>
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
                                         <tr>
-                                            <td>{{ $inventory->name }}</td>
-                                            <td>{{ $inventory->JAN ?? "未設定" }}</td>
-
-
-                                            <td class="text-nowrap">
-                                                <form action="{{ route('inventory.update', $inventory->id) }}"
-                                                      method="POST">
-                                                    @csrf
-                                                    @method('PUT')
-                                                    {{ $inventory->quantity ?? '1' }}
-                                                    <br>
-                                                    <button
-                                                        class="btn btn-outline-{{ Auth::check() && Auth::user()->isDarkMode() ? 'light' : 'dark' }}"
-                                                        name="reduce-btn" type="submit">-
-                                                    </button>
-                                                    <button
-                                                        class="btn btn-outline-{{ Auth::check() && Auth::user()->isDarkMode() ? 'light' : 'dark' }}"
-                                                        name="add-btn" type="submit">+
-                                                    </button>
-                                                </form>
-                                            </td>
-
-
-                                            <td>{{ $inventory->price ?? '未設定' }}</td>
-                                            <td>
-                                                @if($inventory->expiration_date)
-                                                    @if($inventory->expiration_date < date('Y-m-d'))
-                                                        <span style="color: red;">賞味期限切れ：{{ $inventory->expiration_date }}</span>
-                                                    @elseif($inventory->expiration_date === date('Y-m-d'))
-                                                        <span style="color: red;">本日賞味期限</span>
-                                                    @elseif($inventory->expiration_date < date('Y-m-d', strtotime('+3 day')))
-                                                        <span style="color: red;">賞味期限まであと{{ (strtotime($inventory->expiration_date) - strtotime(date('Y-m-d'))) / 86400 }}日 ( {{ $inventory->expiration_date }}</span>
-                                                    @else
-                                                        {{ $inventory->expiration_date }}
-                                                    @endif
-                                                @else
-                                                    未設定
-                                                @endif
-                                            </td>
-                                            <td>{{ $inventory->user_name }}</td>
-                                            <td>{{ $inventory->updated_at }}</td>
-                                            <td class="text-nowrap">
-                                                <a class="btn btn-outline-primary"
-                                                   href="{{ route('inventory.edit', $inventory->id) }}">編集</a>
-                                            </td>
-                                            <td class="text-nowrap">
-                                                <form action="{{ route('inventory.destroy', $inventory->id) }}"
-                                                      method="POST">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button class="btn btn-outline-danger" type="submit">削除</button>
-                                                </form>
-                                            </td>
+                                            <th scope="col">商品名</th>
+                                            <th scope="col">JANコード</th>
+                                            <th scope="col">数</th>
+                                            <th scope="col" class="text-nowrap">価格</th>
+                                            <th scope="col">賞味期限</th>
+                                            <th scope="col">作成者</th>
+                                            <th scope="col">更新日時</th>
+                                            <th scope="col">編集</th>
+                                            <th scope="col">削除</th>
                                         </tr>
-                                        </tbody>
-                                    @endforeach
-                                </table>
+                                        </thead>
+                                        @foreach($inventories as $inventory)
+                                            <tbody>
+                                            <tr>
+                                                <td>{{ $inventory->name }}</td>
+                                                <td>{{ $inventory->JAN ?? "未設定" }}</td>
+
+
+                                                <td class="text-nowrap">
+                                                    <form action="{{ route('inventory.update', $inventory->id) }}"
+                                                          method="POST">
+                                                        @csrf
+                                                        @method('PUT')
+                                                        {{ $inventory->quantity ?? '1' }}
+                                                        <br>
+                                                        <button
+                                                            class="btn btn-outline-{{ Auth::check() && Auth::user()->isDarkMode() ? 'light' : 'dark' }}"
+                                                            name="reduce-btn" type="submit">-
+                                                        </button>
+                                                        <button
+                                                            class="btn btn-outline-{{ Auth::check() && Auth::user()->isDarkMode() ? 'light' : 'dark' }}"
+                                                            name="add-btn" type="submit">+
+                                                        </button>
+                                                    </form>
+                                                </td>
+
+
+                                                <td>{{ $inventory->price ?? '未設定' }}</td>
+                                                <td>
+                                                    @if($inventory->expiration_date)
+                                                        @if($inventory->expiration_date < date('Y-m-d'))
+                                                            <span
+                                                                style="color: red;">賞味期限切れ：{{ $inventory->expiration_date }}</span>
+                                                        @elseif($inventory->expiration_date === date('Y-m-d'))
+                                                            <span style="color: red;">本日賞味期限</span>
+                                                        @elseif($inventory->expiration_date < date('Y-m-d', strtotime('+3 day')))
+                                                            <span style="color: red;">賞味期限まであと{{ (strtotime($inventory->expiration_date) - strtotime(date('Y-m-d'))) / 86400 }}日 ( {{ $inventory->expiration_date }}</span>
+                                                        @else
+                                                            {{ $inventory->expiration_date }}
+                                                        @endif
+                                                    @else
+                                                        未設定
+                                                    @endif
+                                                </td>
+                                                <td>{{ $inventory->user_name }}</td>
+                                                <td>{{ $inventory->updated_at }}</td>
+                                                <td class="text-nowrap">
+                                                    <a class="btn btn-outline-primary"
+                                                       href="{{ route('inventory.edit', $inventory->id) }}">編集</a>
+                                                </td>
+                                                <td class="text-nowrap">
+                                                    <form action="{{ route('inventory.destroy', $inventory->id) }}"
+                                                          method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button class="btn btn-outline-danger" type="submit">削除
+                                                        </button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        @endforeach
+                                    </table>
+                                </div>
                             @endif
                         @endisset
                     </div>
